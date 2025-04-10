4c38d07b28b3e17928205046a19c6dd0194b9c4a
bytenode <bz5167@rit.edu>
2025-04-10 11:20:06 -0400

    Domomaker B part B

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -1,7 +1,35 @@
+const models = require('../models');
+const Domo = models.Domo;
+
 const makerPage = (req, res) => {
   res.render('app');
 };
 
+const makeDomo = async (req, res) => {
+  if (!req.body.name || !req.body.age) {
+    return res.status(400).json({ error: 'Borth name and age are required! '});
+  }
+
+  const domoData = {
+    name: req.body.name,
+    age: req.body.age,
+    owner: req.session.account._id,
+  };
+
+  try {
+    const newDomo = new Domo(domoData);
+    await newDomo.save();
+    return res.json({ redirect: '/maker' });
+  } catch (err) {
+    console.log(err);
+    if (err.code === 11000) {
+      return res.status(400).json({ error: 'Domo already exists! '});
+    }
+    return res.status(500).json({ error: 'An error occurred making domo! '});
+  }
+}
+
 module.exports = {
   makerPage,
+  makeDomo,
 };
